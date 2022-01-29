85a78d42f644d4afe9f61d04ff7ab979d4387344
Jewel522 <hayatliq@hotmail.com>
2022-01-29 11:22:44 -0500

    after creating feature brand1

--- src/test/java/com/cydeo/tests/Jewel_test1.java
+++ src/test/java/com/cydeo/tests/Jewel_test1.java
@@ -3,5 +3,6 @@ package com.cydeo.tests;
 public class Jewel_test1 {
     public static void main(String[] args) {
         System.out.println("Hi from Jewel");
+        //after creating feature brand1
     }
 }
